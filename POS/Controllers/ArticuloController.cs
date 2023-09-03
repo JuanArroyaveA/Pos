cbf82a7b4a308f5545f88e68a78a0c4246adf766
Juan David Arroyave <juanarroyave225751@correo.itm.edu.co>
2023-09-03 13:08:59 -0500

    initial commit

---
+++ POS/Controllers/ArticuloController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArticuloController : ControllerBase
+    {
+    }
+}
---
+++ POS/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/cancion/{id}", (int id) =>
+{
+    return "vale, esta es una api que me trae una canción id = "+ id;
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
